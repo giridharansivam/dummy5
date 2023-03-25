0c946c77b3f03158037b721bde839a123d6be2d1
giridharansivam <54465468+giridharansivam@users.noreply.github.com>
2023-03-25 10:20:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  purge:[
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  mode:'jit',
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
